5ef4a8e4863555c841a3b93f018289216415efca
SummerWRain <zxcvbnmx521jy@163.com>
2021-04-03 18:02:38 +0800

    django

---
+++ mainsite/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
+
+
+# Create your views here.
+
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        now = datetime.now()
+        if slug != None:
+            return render(request, 'blog-single.html', locals())
+    except:
+        return redirect('/')
